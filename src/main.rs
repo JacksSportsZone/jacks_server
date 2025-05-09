5e4d466c4faf84250589da5e15b44e50194ffd4b
Jack Frambes <jframbeees@gmail.com>
2025-05-09 19:18:00 -0400

    Add flexible logger with custom JSON format and rotation
    Integrated a logger using `flexi_logger`, supporting daily log rotation, JSON-formatted entries, and retention of 90 log files. Updated dependencies in `Cargo.toml` to include `flexi_logger`, `log`, and `serde_json`.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,53 @@
 use std::process::exit;
 use std::sync::Arc;
 use actix_web::{App, HttpServer};
-use jacks_sports_zone_api::{setup_logger, start_scheduler, configure_app, shutdown_signal};
+use jacks_sports_zone_api::{ start_scheduler, configure_app, shutdown_signal};
 use jacks_sports_zone_api::college_hockey_zone::database;
+use flexi_logger::{Age, Cleanup, Criterion, DeferredNow, FileSpec, Logger, Naming, WriteMode, Record};
+use serde_json::json;
+
+/// Sets up a logger that writes to both standard output and a daily rotating log file.
+fn setup_logger(log_name: &str) -> Result<(), flexi_logger::FlexiLoggerError> {
+    Logger::try_with_str("info")?
+        .log_to_file(FileSpec::default().directory("logs").basename(log_name))
+        .rotate(
+            Criterion::Age(Age::Day),
+            Naming::Timestamps,
+            Cleanup::KeepLogFiles(90),
+        )
+        .format(custom_format)
+        .write_mode(WriteMode::Direct)
+        .start()?;
+    Ok(())
+}
+
+fn custom_format(
+    w: &mut dyn std::io::Write,
+    now: &mut DeferredNow,
+    record: &Record,
+) -> Result<(), std::io::Error> {
+    let level = if record.target() == "JacksSportsZoneApi" && record.level() == log::Level::Info {
+        "API"
+    } else if record.target() == "JacksSportsZoneApi" && record.level() == log::Level::Debug {
+        "Update"
+    } else if record.target() == "JacksSportsZoneApi" && record.level() == log::Level::Error {
+        "Update_Error"
+    } else {
+        record.level().as_str()
+    };
+
+    let log_entry = json!({
+        "timestamp": now.format("%Y-%m-%dT%H:%M:%S%.6fZ").to_string(),
+        "level": level,
+        "target": record.target(),
+        "file": record.file().unwrap_or("<unnamed>"),
+        "line": record.line().unwrap_or(0),
+        "message": record.args().to_string(),
+    });
+
+    write!(w, "{}", log_entry)?;
+    Ok(())
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
