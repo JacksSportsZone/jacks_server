a9e82e45fd9f907effb986039dc4713b6d92c034
Jack Frambes <jframbeees@gmail.com>
2025-05-31 00:56:35 -0400

    Simplify Notion iframe in App.svelte by removing unused CSS and adjusting dimensions for improved layout.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,27 +3,9 @@
 </script>
 
 <style>
-    .notion-container {
-        position: relative;
-        width: 100%;
-        height: 100vh; /* Adjust as needed */
-        overflow: hidden;
-    }
-
-    .notion-embed {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        border: none;
-    }
 </style>
 
 <div class="notion-container">
-    <iframe
-            allowfullscreen
-            class="notion-embed"
-            src="https://few-salto-24b.notion.site/ebd/2032e79a56a580648b57fcf12888b49d?v=2032e79a56a5805aa64a000ca21ff5fb"
-    ></iframe>
+    <iframe allowfullscreen
+            frameborder="0" height="600" src="https://few-salto-24b.notion.site/ebd/2032e79a56a580648b57fcf12888b49d?v=2032e79a56a5805aa64a000ca21ff5fb" width="100%"/>
 </div>
